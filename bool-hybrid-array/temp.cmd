c6a8b44b899689abb16428ef611c522e661ecc89
BKsell <1289270215@qq.com>
2025-10-25 21:52:50 +0800

    Add files via upload

---
+++ bool-hybrid-array/temp.cmd
@@ -0,0 +1,14 @@
+python -OO temp.py
+python -O temp.py
+python temp.py
+python -m bool_hybrid_array.__init__
+python -O -m bool_hybrid_array.__init__
+python -OO -m bool_hybrid_array.__init__
+
+
+
+
+
+python -OO -m bool_hybrid_array
+python -OO -m ≤‚ ‘¥˙¬Î.py
+pause
---
+++ bool-hybrid-array/temp.py
@@ -0,0 +1,2 @@
+import bool_hybrid_array
+from bool_hybrid_array import *
